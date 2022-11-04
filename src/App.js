76479e055967fa2d8992e23a75134e62f5568a9d
saisumit <sumitvohra90@gmail.com>
2022-11-04 16:17:34 +0530

    create a beryl test

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
-function openDrawer(){
-  window.__gamePay("event","open");
-}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={openDrawer}>
-          CLICK ME
-        </button>
       </header>
     </div>
   );
