81231a64b2a0658c4bd6879a0ccb244f834437a6
utkarsh <utkarsh.bharti30@gmail.com>
2023-03-14 16:58:58 +0530

    Revert "Revert "Fix/resolve conflict branch from sandbox""

--- src/components/NFTCard/index.js
+++ src/components/NFTCard/index.js
@@ -1,6 +1,11 @@
 import { Box, Button, Divider, Typography } from '@mui/material';
-
-export default function NFTCard({ nft }) {
+import axios from 'axios';
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import hmacSHA512 from 'crypto-js/hmac-sha512';
+import Hex from 'crypto-js/enc-hex';
+import s9yNft from '../../assets/s9ynft.jpeg';
+export default function NFTCard({ nft, userId }) {
   const {
     balance,
     contractMetadata: { symbol },
@@ -10,6 +15,72 @@ export default function NFTCard({ nft }) {
   } = nft;
 
   const image = media[0].thumbnail;
+
+  const tokens = [
+    { value: 66, label: 'USDC' },
+    { value: 55, label: 'MATIC' },
+  ];
+  const [token, setToken] = useState('55');
+  const [amount, setAmount] = useState('0.1');
+  const [assetQuantity, setAssetQuantity] = useState('1');
+  const [assetId, setAssetId] = useState('111');
+  const [loading, setLoading] = useState(false);
+
+  const initiateTransaction = async () => {
+    setLoading(true);
+
+    try {
+      const clientReferenceId = uuidv4();
+
+      const body = {
+        clientReferenceId,
+        userId,
+        singularityTransactionType: 'EXCHANGE',
+        clientReceiveObject: {
+          clientRequestedAssetId: token,
+        },
+        clientExchangeAssetDetailsList: [
+          {
+            userRequestedAssetQuantity: assetQuantity,
+            userRequestedAssetId: assetId,
+            clientRequestedAssetQuantity: amount,
+            userRequestedNFTTokenId: '0',
+            singularityTransactionExchangeMode: 'CONTRACT',
+          },
+        ],
+      };
+
+      const secret =
+        'SSk49aq1/kQ1eKH7Sg+u4JsisvrycRcLopHdM6lNEMVe/p7lsSVoRiY0neFYNJkHoWVEK30bPAV2pNU2WwOJXQ==';
+      const signature = Hex.stringify(hmacSHA512(JSON.stringify(body), secret));
+
+      const cerebro_prefix = 'https://cerebro.s9y';
+      let baseEndpoint = `${cerebro_prefix}-qal.com`; // default qal env
+      if (process.env.REACT_APP_ENV === 'production') {
+        baseEndpoint = `${cerebro_prefix}.gg`;
+      } else if (process.env.REACT_APP_ENV === 'sandbox') {
+        baseEndpoint = `${cerebro_prefix}-sandbox.gg`;
+      }
+
+      const res = await axios.post(
+        `${baseEndpoint}/v1/sdk-server-protected/create_singularity_initial_txn`,
+        body,
+        {
+          headers: {
+            'x-api-key': 2,
+            'X-api-signature': signature,
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+      console.log('nft create txn api response --->', res);
+      window.SingularityEvent.nftFlow('INIT_NFT_PAYMENT', res.data);
+    } catch (err) {
+      window.alert('Some error occured');
+      console.error(err);
+    }
+    setLoading(false);
+  };
 
   return (
     <Box
@@ -22,7 +93,7 @@ export default function NFTCard({ nft }) {
       }}
     >
       <Box textAlign="center" my={1}>
-        <img src={image} alt="" height="100px" />
+        <img src={s9yNft} alt="" height="100px" />
       </Box>
 
       <Box color="white">
@@ -64,8 +135,9 @@ export default function NFTCard({ nft }) {
               whiteSpace: 'nowrap',
             }}
             variant="contained"
+            onClick={initiateTransaction}
           >
-            buy now
+            {loading ? 'Processing...' : 'buy now'}
           </Button>
         </Box>
       </Box>
