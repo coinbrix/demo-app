b49b9bd18f977525030bff2520d2f767854e9470
Amit Sharma <amit@neobrix.io>
2023-03-26 23:17:13 +0530

    Transactions changes amit wip

--- src/components/NFTCard/index.js
+++ src/components/NFTCard/index.js
@@ -50,31 +50,32 @@ export default function NFTCard({ nft, userId }) {
         ],
       };
 
-      const secret =
-        'SSk49aq1/kQ1eKH7Sg+u4JsisvrycRcLopHdM6lNEMVe/p7lsSVoRiY0neFYNJkHoWVEK30bPAV2pNU2WwOJXQ==';
-      const signature = Hex.stringify(hmacSHA512(JSON.stringify(body), secret));
+      const secret = 'SSk49aq1/kQ1eKH7Sg+u4JsisvrycRcLopHdM6lNEMVe/p7lsSVoRiY0neFYNJkHoWVEK30bPAV2pNU2WwOJXQ==';
+      const requestString = JSON.stringify(body)
+      const signature = Hex.stringify(hmacSHA512(requestString, secret));
 
-      const cerebro_prefix = 'https://cerebro.s9y';
-      let baseEndpoint = `${cerebro_prefix}-qal.com`; // default qal env
-      if (process.env.REACT_APP_ENV === 'production') {
-        baseEndpoint = `${cerebro_prefix}.gg`;
-      } else if (process.env.REACT_APP_ENV === 'sandbox') {
-        baseEndpoint = `${cerebro_prefix}-sandbox.gg`;
-      }
+      // const cerebro_prefix = 'https://cerebro.s9y';
+      // let baseEndpoint = `${cerebro_prefix}-qal.com`; // default qal env
+      // if (process.env.REACT_APP_ENV === 'production') {
+      //   baseEndpoint = `${cerebro_prefix}.gg`;
+      // } else if (process.env.REACT_APP_ENV === 'sandbox') {
+      //   baseEndpoint = `${cerebro_prefix}-sandbox.gg`;
+      // }
 
-      const res = await axios.post(
-        `${baseEndpoint}/v1/sdk-server-protected/create_singularity_initial_txn`,
-        body,
-        {
-          headers: {
-            'x-api-key': 2,
-            'X-api-signature': signature,
-            'Content-Type': 'application/json',
-          },
-        }
-      );
-      console.log('nft create txn api response --->', res);
-      window.SingularityEvent.nftFlow('INIT_NFT_PAYMENT', res.data);
+      // const res = await axios.post(
+      //   `${baseEndpoint}/v1/sdk-server-protected/create_singularity_initial_txn`,
+      //   body,
+      //   {
+      //     headers: {
+      //       'x-api-key': 2,
+      //       'X-api-signature': signature,
+      //       'Content-Type': 'application/json',
+      //     },
+      //   }
+      // );
+      // console.log('nft create txn api response --->', res);
+      // window.SingularityEvent.nftFlow('INIT_NFT_PAYMENT', res.data);
+      window.SingularityEvent.transactionFlowNew(requestString, signature);
     } catch (err) {
       window.alert('Some error occured');
       console.error(err);
