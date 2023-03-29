135de4b69da0cd9bf8c9ad2b87fa4fd84d33b218
Amit Sharma <amit@neobrix.io>
2023-03-29 07:12:44 +0530

    updated transaction flow

--- src/components/TransactionCard/index.js
+++ src/components/TransactionCard/index.js
@@ -58,27 +58,7 @@ export default function TransactionCard({ userId }) {
 
       const requestString = JSON.stringify(body)
       const signature = Hex.stringify(hmacSHA512(requestString, secret));
-
-      // const cerebro_prefix = 'https://cerebro.s9y';
-      // let baseEndpoint = `${cerebro_prefix}-qal.com`; // default qal env
-      // if (process.env.REACT_APP_ENV === 'production') {
-      //   baseEndpoint = `${cerebro_prefix}.gg`;
-      // } else if (process.env.REACT_APP_ENV === 'sandbox') {
-      //   baseEndpoint = `${cerebro_prefix}-sandbox.gg`;
-      // }
-      // const res = await axios.post(
-      //   `${baseEndpoint}/v1/sdk-server-protected/create_singularity_initial_txn`,
-      //   body,
-      //   {
-      //     headers: {
-      //       'x-api-key': 2,
-      //       'X-api-signature': signature,
-      //       'Content-Type': 'application/json',
-      //     },
-      //   }
-      // );
-
-      window.SingularityEvent.transactionFlowNew(requestString, signature);
+      window.SingularityEvent.transactionFlow(requestString, signature);
     } catch (err) {
       window.alert('Some error occured');
       console.error(err);
