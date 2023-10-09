2e65dc1ff05306b35636b94f01da327ea3f3cd7e
Amit Sharma <amit@neobrix.io>
2023-10-09 12:43:49 +0530

    fix: udpated buying nft payload

--- src/components/BuyNFT/index.js
+++ src/components/BuyNFT/index.js
@@ -55,11 +55,19 @@ export default function BuyNFT() {
     try {
       const clientReferenceId = uuidv4();
 
+      const userData = await window.SingularityEvent.getConnectUserInfo()
+      if(!userData.metaData){
+        alert('error fetching user data')
+        return
+      }
+      const publicAddress = userData.metaData.wallet.accounts.evmPublicAddress[0].publicAddress
+
       let body = {
         clientReferenceId,
         singularityTransactionType: 'NFT_PURCHASE',
         clientReceiveObject: {
           clientRequestedAssetId: clientRequestedAssetTd,
+          address: publicAddress
         },
         transactionIconLink: 'https://singularity-web-assets-public.s3.ap-south-1.amazonaws.com/s9ynft.jpeg',
         transactionLabel: 'S9Y NFT',
--- src/components/BuySingleNFT/index.js
+++ src/components/BuySingleNFT/index.js
@@ -97,6 +97,13 @@ export default function BuySingleNFT() {
     try {
       const clientReferenceId = uuidv4();
 
+      const userData = await window.SingularityEvent.getConnectUserInfo()
+      if(!userData.metaData){
+        alert('error fetching user data')
+        return
+      }
+      const publicAddress = userData.metaData.wallet.accounts.evmPublicAddress[0].publicAddress
+
       let body = {
         clientReferenceId,
         singularityTransactionType: 'NFT_PURCHASE',
@@ -104,6 +111,7 @@ export default function BuySingleNFT() {
         transactionLabel: 'S9Y NFT',
         clientReceiveObject: {
           clientRequestedAssetId: clientRequestedAssetTd,
+          address: publicAddress
         },
         userReceiveAssetDetailsList: [
           {
