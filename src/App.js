fceeb94c7c70764b57f971f85d1f80fef0141047
konitroos <konitroosop@gmail.com>
2023-12-27 13:44:06 +0530

    fix: optimisation changes

--- src/App.js
+++ src/App.js
@@ -73,9 +73,9 @@ function App() {
           }
         );
 
-        await checkLoginAndAction()
-
         setLoading(false);
+
+        await checkLoginAndAction();
 
         const isAutomation = new URLSearchParams(window?.location?.search)?.get('isAutomation');
 
