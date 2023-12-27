f3af5e23ae6cd3718da7c329061800115cca6087
konitroos <konitroosop@gmail.com>
2023-12-27 12:36:26 +0530

    fix: update loader state

--- src/App.js
+++ src/App.js
@@ -77,9 +77,9 @@ function App() {
           }
         );
 
-        await checkLoginAndAction()
-
         setLoading(false);
+
+        await checkLoginAndAction();
 
         const isAutomation = new URLSearchParams(window?.location?.search)?.get('isAutomation');
 
