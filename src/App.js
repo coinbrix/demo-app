03cf6b73240687e50ece26921597427e29fd1032
konitroos <konitroosop@gmail.com>
2024-01-06 23:59:51 +0530

    fix: user login check

--- src/App.js
+++ src/App.js
@@ -72,11 +72,10 @@ function App() {
             console.log('Txn failed', JSON.parse(data));
           }
         );
-
-        
-        await checkLoginAndAction()
         
         setLoading(false);
+
+        await checkLoginAndAction();
 
         const isAutomation = new URLSearchParams(window?.location?.search)?.get('isAutomation');
 
